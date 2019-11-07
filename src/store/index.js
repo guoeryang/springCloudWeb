628a3e6ca2e64901fa0d75d04dc3ef9803296842
郭二阳 <446205921@qq.com>
2019-11-08 01:51:22 +0800

    up

--- src/store/index.js
+++ src/store/index.js
@@ -1,23 +1,23 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import getters from './getters'
-import api from './modules/api'
 import app from './modules/app'
+import api from './modules/api'
+import user from './modules/user'
 import tagsView from './modules/tagsView'
 import settings from './modules/settings'
-import user from './modules/user'
 import permission from './modules/permission'
+import getters from './getters'
 
 Vue.use(Vuex)
 
 const store = new Vuex.Store({
   modules: {
-    api,
     app,
-    tagsView,
-    settings,
+    api,
     user,
-    permission
+    tagsView,
+    permission,
+    settings
   },
   getters
 })
